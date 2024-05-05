d0653799534b5444cc900424a6ae48e2ab8a2b65
Muhammed Mishab <mishabdotz@gmail.com>
2024-05-05 19:58:19 +0530

    first commit

---
+++ src/assets/Components/Header.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') return;
+    setTodos([...todos, { id: Date.now(), text: newTodo }]);
+    setNewTodo('');
+  };
+
+  const removeTodo = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Header from './assets/Components/Header'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Header />
+
+  </React.StrictMode>,
+) 
